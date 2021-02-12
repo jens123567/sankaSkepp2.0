32df5c8cf977c9c5c824155bc05fae519a4cf693
jens123567 <jens.malmqvist03@gmail.com>
2021-02-12 16:24:31 +0100

    overhaul better system

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const rows = 10;
 const columns = 10;
 const cellSize = "50px";
 
+let boatOrientation = 0;
+let gamemode1 = 0;
+let boatType1 = 0;
+let selected1 = [];
+
+// generates board 1
 const grid1 = new Array(10).fill(0).map(() => new Array(10).fill(0));
 
-// genererar mall för objekt
-const square = {
-  selected: false,
-};
-
-// genererar brädet
 const battleBoard1 = document.querySelector("#board1");
 
 battleBoard1.style.display = "grid";
@@ -20,35 +20,53 @@ battleBoard1.style.gridTemplateRows = new Array(rows).fill(cellSize).join(" ");
 
 for (let x = 0; x < columns; x++) {
   for (let y = 0; y < rows; y++) {
-    const cell = document.createElement("div");
-    cell.id = `s:${x}:${y}`;
-    let cellid = `s:${x}:${y}`;
+    const cell1 = document.createElement("div");
+    battleBoard1.appendChild(cell1);
 
-    const cellObject = Object.create(square);
+    cell1.id = `a:${x}:${y}`;
+    let cellid = `a:${x}:${y}`;
+    cell1.innerHTML = `${x}:${y}`;
 
     grid1[x][y] = cellid;
 
-    battleBoard1.appendChild(cell);
-    cell.addEventListener("click", function () {
-      let boatLength = document.getElementById("boatLength").value;
-
-      if (cellObject.selected == true) {
-        for (let i = 0; i < boatLength; i++) {
-          cellid = `s:${x + i}:${y}`;
-          cellObject.selected = false;
-          document.getElementById(cellid).style.backgroundColor = "#5d9ad4";
-        }
-        cellObject.selected = false;
-        document.getElementById(cellid).style.backgroundColor = "#5d9ad4";
-      } else {
-        for (let i = 0; i < boatLength; i++) {
-          cellid = `s:${x + i}:${y}`;
-          cellObject.selected = true;
-          document.getElementById(cellid).style.backgroundColor = "red";
-        }
-      }
+    cell1.addEventListener("click", () => {
+      click(x, y);
     });
+  }
+}
 
-    cell.innerHTML = `s:${x}:${y}`;
+// is run when cell is clicked
+function click(x, y) {
+  if (gamemode1 == 0) {
+    placeBoats(x, y);
+  } else {
+    console.log("gamemode not here");
+  }
+}
+
+// place boats
+function placeBoats(x, y) {
+  switch (boatType1) {
+    case 0:
+      placeCarrier(x, y);
+      break;
+
+    default:
+      break;
+  }
+}
+
+function placeCarrier(x, y) {
+  if (boatOrientation == 0) {
+    if (selected1.includes(grid1[x][y])) {
+      console.log("error");
+    } else {
+      for (let i = 0; i < 5; i++) {
+        document.getElementById(grid1[x + i][y]).style.backgroundColor = "red";
+        let newLength = selected1.push(`a:${x}:${y}`);
+        console.log(selected1);
+      }
+    }
+  } else {
   }
 }
