da4f0666501de053052618f7f3798dd7963a3b63
jens123567 <jens.malmqvist03@gmail.com>
2021-02-19 16:28:09 +0100

    comit

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const columns = 10;
 const cellSize = "50px";
 
 let boatOrientation = 0;
+let boatType1 = 1;
 let gamemode1 = 0;
-let boatType1 = 0;
 let selected1 = [];
 
 // generates board 1
@@ -37,6 +37,8 @@ for (let x = 0; x < columns; x++) {
 
 // is run when cell is clicked
 function click(x, y) {
+  console.log(boatType1);
+  console.log("klickade");
   if (gamemode1 == 0) {
     placeBoats(x, y);
   } else {
@@ -51,17 +53,43 @@ function placeBoats(x, y) {
       placeCarrier(x, y);
       break;
 
+    case 1:
+      placeCruiser(x, y);
+      break;
+
     default:
       break;
   }
 }
 
+// places carrier (5 long boat)
 function placeCarrier(x, y) {
   if (boatOrientation == 0) {
     if (selected1.includes(grid1[x][y])) {
       console.log("error");
     } else {
       for (let i = 0; i < 5; i++) {
+        document.getElementById(grid1[x + i][y]).style.backgroundColor = "red";
+        let newLength = selected1.push(`a:${x}:${y}`);
+        console.log(selected1);
+      }
+    }
+  } else {
+  }
+}
+
+// places cruiser (4 long boat)
+function placeCruiser(x, y) {
+  if (boatOrientation == 0) {
+    if (
+      new Array(5)
+        .fill(0)
+        .map((_, i) => i)
+        .map((v) => selected1.includes(grid1[x + v][y])(true))
+    ) {
+      console.log("error");
+    } else {
+      for (let i = 0; i < 4; i++) {
         document.getElementById(grid1[x + i][y]).style.backgroundColor = "red";
         let newLength = selected1.push(`a:${x}:${y}`);
         console.log(selected1);
